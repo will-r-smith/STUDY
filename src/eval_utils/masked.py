b51fdcb6d960be4ee320925ec6cb2b13126ffe96
will-r-smith <wrs0209@gmail.com>
2024-07-27 15:44:04 +0100

    re

--- src/eval_utils/masked.py
+++ src/eval_utils/masked.py
@@ -12,15 +12,9 @@ def generate_outputs(self, model, X, y, requires_grad, get_accuracy):
 
     answers = [gold_answer if gold_answer.startswith(" ") else f" {gold_answer}" for gold_answer in y]
 
-    print(self.tokenizer(answers)["input_ids"])
 
     answer_ids = [self.tokenizer(answer)["input_ids"][1] for answer in answers]
-
-    answer_ids = torch.LongTensor(answer_ids).unsqueeze(1).to(self.device)
-
-    print(answer_ids)
-
-    answer_ids = answer_ids[:,0]
+    answer_ids = torch.LongTensor(answer_ids).unsqueeze(1).to(self.device)[:,0]
 
     if requires_grad == False:
         with torch.no_grad():
@@ -28,19 +22,13 @@ def generate_outputs(self, model, X, y, requires_grad, get_accuracy):
     else:
         logits = model(**input_ids).logits
 
-    print(answer_ids)
-
     mask_ids = mask_ids.view(my_batch_size, 1, 1).expand([my_batch_size, 1, logits.shape[2]])
     masked_logits = torch.gather(logits, index=mask_ids, dim=1)
     loss = torch.nn.CrossEntropyLoss()(masked_logits[:,-1,:], answer_ids)
 
     
-
     if get_accuracy == True:
         top_tokens = torch.topk(masked_logits, 10, dim=-1).indices  # shape: (batch_size, top_k)
-
-        top1_predictions = top_tokens[:, 0]
-        print(top1_predictions)
 
         top1_correct = (top_tokens[:,0,0] == answer_ids).sum().item()
         top10_correct = sum([answer_ids[j].item() in top_tokens[j].tolist() for j in range(len(answer_ids))])
