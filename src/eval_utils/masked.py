71e561e949a86acd9e2a29057d5cc5c496a99f6e
will-r-smith <wrs0209@gmail.com>
2024-08-06 17:06:19 +0100

    re

--- src/eval_utils/masked.py
+++ src/eval_utils/masked.py
@@ -36,11 +36,12 @@ def generate_outputs(self, model_eval, X, y, requires_grad, get_accuracy):
 
     if get_accuracy == True:
         top_tokens = torch.topk(masked_logits, 10, dim=-1).indices  # shape: (batch_size, top_k)
+        top1_predictions = top_tokens[:,0,0]
 
-        top1_correct = (top_tokens[:,0,0] == answer_ids).sum().item()
+        top1_correct = (top1_predictions == answer_ids).sum().item()
         top10_correct = sum([answer_ids[j].item() in top_tokens[j,:,0].tolist() for j in range(len(answer_ids))])
         
-        top1_words = [self.tokenizer.decode([token]) for token in top_tokens]
+        top1_words = [self.tokenizer.decode([token]) for token in top1_predictions]
         top10_words = [[self.tokenizer.decode([token]) for token in tokens] for tokens in top_tokens]
 
 
